ef1124ba05b4e35800e74b6b7b713a338a1419cb
Jin1c-3 <126029323+Jin1c-3@users.noreply.github.com>
2023-10-26 19:52:23 +0800

    增加一键登录验证

--- DBFactory.py
+++ DBFactory.py
@@ -14,27 +14,58 @@ class DBFactory:
         self.cur.close()
         self.conn.close()
 
-    def do(self, sql):
-        self.cur.execute(sql)
+    def do(self, sql, *value):
+        """
+        @param sql: sql语句
+        @param value: sql语句中的参数，是可变长的
+        """
+        self.cur.execute(sql, value)
+        self.conn.commit()
         return self.cur.fetchall()
 
+    def validate_username_password(self, username, password):
+        """
+        len长度大于零就是对的
+        """
+        return (
+            len(
+                self.do(
+                    "select * from users where username='?' and pwd='?'",
+                    username,
+                    password,
+                )
+            )
+            > 0
+        )
+
     def get_fields(self, table_name):
+        """
+        @param table_name: 表名
+        获取表的字段名
+        """
         sql = f"PRAGMA table_info({table_name})"
         fields = [feild[1] for feild in self.do(sql)]
         return fields
 
     def all(self, table_name):
+        """
+        @param table_name: 表名
+        获取表的所有数据
+        """
         sql = f"select * from {table_name}"
         return self.do(sql)
 
     def update(self, data: dict, table_name):
+        """
+        @param data: 数据的第一个字段必须是id，字段名可以不是id
+        """
         values = []
         id_field_name = list(data)[0]
         for key in list(data)[1:]:
             values.append(f"{key}='{data[key]}'")
-        sql = f"update {table_name} set {','.join(values)} where {id_field_name}=\"{data[id_field_name]}\""
+        sql = f"update {table_name} set {','.join(values)} where {id_field_name}='?'"
         # print (sql)
-        self.cur.execute(sql)
+        self.cur.execute(sql, data[id_field_name])
         self.conn.commit()
 
     def insert(self, data: dict, table_name):
@@ -54,81 +85,81 @@ class DBFactory:
         self.conn.commit()
 
 
-def init_db(dbname="./db/student_083_2.db"):
-    conn = sqlite3.connect(dbname)
-    return conn
+# def init_db(dbname="./db/student_083_2.db"):
+#     conn = sqlite3.connect(dbname)
+#     return conn
 
 
-def GetSql(conn, sql):
-    cur = conn.cursor()
-    cur.execute(sql)
-    fields = []
-    for field in cur.description:
-        fields.append(field[0])
+# def GetSql(conn, sql):
+#     cur = conn.cursor()
+#     cur.execute(sql)
+#     fields = []
+#     for field in cur.description:
+#         fields.append(field[0])
 
-    result = cur.fetchall()
-    # for item in result:
-    #     print(item)
-    cur.close()
-    return result, fields
-
-
-def CloseDb(conn):
-    conn.close()
+#     result = cur.fetchall()
+#     # for item in result:
+#     #     print(item)
+#     cur.close()
+#     return result, fields
 
 
-def GetSql2(sql):
-    conn = init_db()
-    result, fields = GetSql(conn, sql)
-    CloseDb(conn)
-    return result, fields
+# def CloseDb(conn):
+#     conn.close()
 
 
-def UpdateData(data, tablename):
-    conn = init_db()
-    values = []
-    cusor = conn.cursor()
-    idName = list(data)[0]
-    for v in list(data)[1:]:
-        values.append("%s='%s'" % (v, data[v]))
-    sql = "update %s set %s where %s='%s'" % (
-        tablename,
-        ",".join(values),
-        idName,
-        data[idName],
-    )
-    # print (sql)
-    cusor.execute(sql)
-    conn.commit()
-    CloseDb(conn)
+# def GetSql2(sql):
+#     conn = init_db()
+#     result, fields = GetSql(conn, sql)
+#     CloseDb(conn)
+#     return result, fields
 
 
-def InsertData(data, tablename):
-    conn = init_db()
-    values = []
-    cusor = conn.cursor()
-    fieldNames = list(data)
-    for v in fieldNames:
-        values.append(data[v])
-    sql = "insert into  %s (%s) values( %s) " % (
-        tablename,
-        ",".join(fieldNames),
-        ",".join(["?"] * len(fieldNames)),
-    )
-    # print(sql)
-    cusor.execute(sql, values)
-    conn.commit()
-    CloseDb(conn)
+# def UpdateData(data, tablename):
+#     conn = init_db()
+#     values = []
+#     cusor = conn.cursor()
+#     idName = list(data)[0]
+#     for v in list(data)[1:]:
+#         values.append("%s='%s'" % (v, data[v]))
+#     sql = "update %s set %s where %s='%s'" % (
+#         tablename,
+#         ",".join(values),
+#         idName,
+#         data[idName],
+#     )
+#     # print (sql)
+#     cusor.execute(sql)
+#     conn.commit()
+#     CloseDb(conn)
 
 
-def DelDataById(id, value, tablename):
-    conn = init_db()
-    values = []
-    cusor = conn.cursor()
+# def InsertData(data, tablename):
+#     conn = init_db()
+#     values = []
+#     cusor = conn.cursor()
+#     fieldNames = list(data)
+#     for v in fieldNames:
+#         values.append(data[v])
+#     sql = "insert into  %s (%s) values( %s) " % (
+#         tablename,
+#         ",".join(fieldNames),
+#         ",".join(["?"] * len(fieldNames)),
+#     )
+#     # print(sql)
+#     cusor.execute(sql, values)
+#     conn.commit()
+#     CloseDb(conn)
 
-    sql = "delete from %s  where %s=?" % (tablename, id)
-    # print (sql)
 
-    cusor.execute(sql, (value,))
-    conn.commit()
-    CloseDb(conn)
+# def DelDataById(id, value, tablename):
+#     conn = init_db()
+#     values = []
+#     cusor = conn.cursor()
+
+#     sql = "delete from %s  where %s=?" % (tablename, id)
+#     # print (sql)
+
+#     cusor.execute(sql, (value,))
+#     conn.commit()
+#     CloseDb(conn)
--- main.py
+++ main.py
@@ -1,133 +1,128 @@
 from flask import Flask, render_template, request, url_for, redirect, session
 
-from dbSqlite3 import *
+from DBFactory import *
 
 app = Flask(__name__)
-app.secret_key = 'abcdefgh!@#$%'
+app.secret_key = "abcdefgh!@#$%"
+dbf = DBFactory()
+
 
 def CheckLogin():
-    if 'userid' not in session:
+    if "userid" not in session:
         return False
     else:
         return True
 
-@app.route('/login', methods=['POST', 'GET'])
+
+@app.route("/login", methods=["POST", "GET"])
 def login():
-
-    if request.method=="GET":
-        return render_template('login.html')
-    if 1 ==1 :
-
-        result, _ = GetSql2("select * from users where username='%s' and pwd='%s'" % (request.form['username'],request.form['pwd']))
-        print(result)
-        if len(result) > 0:
-            session["userid"]=result[0][0]
-            return redirect(url_for('index'))
+    if request.method == "GET":
+        return render_template("login.html")
+    if 1 == 1:
+        if dbf.validate_username_password(
+            request.form["username"], request.form["password"]
+        ):
+            session["userid"] = request.form["username"]
+            return redirect(url_for("index"))
         else:
-            return render_template('login.html')
+            return render_template("login.html")
 
 
-@app.route('/', methods=['GET'])
+@app.route("/", methods=["GET"])
 def index():
     if not CheckLogin():
-        return redirect(url_for('login'))
-
+        return redirect(url_for("login"))
     tablename = "student_info"
-    sql = "SELECT s.*,  p.stu_profession FROM student_info s INNER JOIN stu_profession p " \
-          "on s.stu_profession_id=p.stu_profession_id"
-
+    sql = (
+        "SELECT s.*,  p.stu_profession FROM student_info s INNER JOIN stu_profession p "
+        "on s.stu_profession_id=p.stu_profession_id"
+    )
     strWhere = []
     if "name" in request.args:
         name = request.args["name"]
         if name != "":
             strWhere.append("stu_name like '%%%s%%'" % name)
-
     if "stuno" in request.args:
         stuno = request.args["stuno"]
         if stuno != "":
             strWhere.append("stu_id = '%s'" % stuno)
-
-    if len(strWhere)>0:
-        sql=sql + " where " + " and ".join(strWhere)
-        print (sql)
-
-
-    result, fields = GetSql2(sql)
-    return render_template('show1.html', datas=result, fields=fields)
+    if len(strWhere) > 0:
+        sql = sql + " where " + " and ".join(strWhere)
+        print(sql)
+    result, fields = dbf.do(sql)
+    return render_template("show1.html", datas=result, fields=fields)
 
 
-@app.route('/add', methods=['GET', 'post'])
+@app.route("/add", methods=["GET", "post"])
 def add():
     if not CheckLogin():
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
 
     if request.method == "GET":
-        datas, _ = GetSql2("select * from stu_profession")
-        return render_template('add.html', datas=datas)
+        datas, _ = dbf.do("select * from stu_profession")
+        return render_template("add.html", datas=datas)
 
     else:
         data = dict(
-            stu_id=request.form['stu_id'],
-            stu_name=request.form['stu_name'],
-            stu_sex=request.form['stu_sex'],
-            stu_age=request.form['stu_age'],
-            stu_origin=request.form['stu_origin'],
-            stu_profession_id=request.form['stu_profession']
+            stu_id=request.form["stu_id"],
+            stu_name=request.form["stu_name"],
+            stu_sex=request.form["stu_sex"],
+            stu_age=request.form["stu_age"],
+            stu_origin=request.form["stu_origin"],
+            stu_profession_id=request.form["stu_profession"],
         )
-
-        InsertData(data, "student_info")
+        dbf.insert(data, "student_info")
         return redirect(url_for("index"))
 
 
-@app.route('/del', methods=['GET'])
+@app.route("/del", methods=["GET"])
 def delete():
     if not CheckLogin():
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
     id = request.args["stuid"]
-    DelDataById("stu_id", id, "student_info")
+    dbf.delete_by_id("stu_id", id, "student_info")
     return redirect(url_for("index"))
 
 
-@app.route('/del2/<id>', methods=['GET'])
+@app.route("/del2/<id>", methods=["GET"])
 def delete2(id):
     if not CheckLogin():
-        return redirect(url_for('login'))
-    DelDataById("stu_id", id, "student_info")
+        return redirect(url_for("login"))
+    dbf.delete_by_id("stu_id", id, "student_info")
     return redirect(url_for("index"))
 
 
-@app.route('/update', methods=['GET', 'post'])
+@app.route("/update", methods=["GET", "post"])
 def upadte():
     if not CheckLogin():
-        return redirect(url_for('login'))
+        return redirect(url_for("login"))
 
     if request.method == "GET":
-        id = request.args['id']
-        result, _ = GetSql2("select * from student_info where stu_id='%s'" % id)
+        id = request.args["id"]
+        result = dbf.do("select * from student_info where stu_id='%s'" % id)
         # result, _ = GetSql2("SELECT s.*,  p.stu_profession FROM student_info s INNER JOIN stu_profession p " \
         #                     "on s.stu_profession_id=p.stu_profession_id where stu_id='%s'" % id)
         print(result[0])
         print(type(result[0]))
-        datas, _ = GetSql2("select * from stu_profession")
+        datas = dbf.all("stu_profession")
         # for p in pro:
         #     print(p)+
-        return render_template('update.html', data=result[0],datas=datas)
+        return render_template("update.html", data=result[0], datas=datas)
     else:
-
         data = dict(
-            stu_id=request.form['stu_id'],
-            stu_name=request.form['stu_name'],
-            stu_sex=request.form['stu_sex'],
-            stu_age=request.form['stu_age'],
-            stu_origin=request.form['stu_origin'],
-            stu_profession_id=request.form['stu_profession']
+            stu_id=request.form["stu_id"],
+            stu_name=request.form["stu_name"],
+            stu_sex=request.form["stu_sex"],
+            stu_age=request.form["stu_age"],
+            stu_origin=request.form["stu_origin"],
+            stu_profession_id=request.form["stu_profession"],
         )
-        UpdateData(data, "student_info")
+        dbf.update(data, "student_info")
 
         return redirect(url_for("index"))
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     # app.run("0.0.0.0",debug=True)
     app.run(debug=True)
     # app.run()
